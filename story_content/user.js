c49ae7b19b9256db7abd1670befb9fc0f7dee726
Elad Daniel <103339350+Eladniell@users.noreply.github.com>
2023-04-20 13:52:06 +0300

    200423

---
+++ story_content/user.js
@@ -0,0 +1,382 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5g3mjdPsFHP":
+        Script1();
+        break;
+      case "6b0Q5HE9ekG":
+        Script2();
+        break;
+      case "6dhfjjUitkF":
+        Script3();
+        break;
+      case "6o0PaZeuAKv":
+        Script4();
+        break;
+      case "6EvYdoFJD60":
+        Script5();
+        break;
+      case "6mvRa70GtSu":
+        Script6();
+        break;
+      case "5VWJHtnHSYq":
+        Script7();
+        break;
+      case "6cuodYy21jn":
+        Script8();
+        break;
+      case "64E4X5dhrvQ":
+        Script9();
+        break;
+      case "6rPYIVKJXeT":
+        Script10();
+        break;
+      case "6fWJJ29stKC":
+        Script11();
+        break;
+      case "5iovw5ttGRx":
+        Script12();
+        break;
+      case "6Z2YyIzFBgI":
+        Script13();
+        break;
+      case "6cmNAjVksNP":
+        Script14();
+        break;
+      case "5loYoRSb09k":
+        Script15();
+        break;
+      case "63GShZ7WmyL":
+        Script16();
+        break;
+      case "6qS0n1aOG69":
+        Script17();
+        break;
+      case "5iJX3oCYCZF":
+        Script18();
+        break;
+      case "63gfYNmiMhU":
+        Script19();
+        break;
+      case "6njh76bifly":
+        Script20();
+        break;
+      case "5YT9Ah2YL7Z":
+        Script21();
+        break;
+      case "6GC3zAt8p7U":
+        Script22();
+        break;
+      case "6E0lyb9aOXj":
+        Script23();
+        break;
+      case "6n1pk29sC7e":
+        Script24();
+        break;
+      case "5yzciyTJRTX":
+        Script25();
+        break;
+      case "5Xi1BnPqWuO":
+        Script26();
+        break;
+      case "6hBeCOi8n3S":
+        Script27();
+        break;
+      case "5k5Avo5GWCa":
+        Script28();
+        break;
+      case "6qOtX446lJo":
+        Script29();
+        break;
+      case "5bOj82hsqxy":
+        Script30();
+        break;
+      case "5VsiYMLevMq":
+        Script31();
+        break;
+      case "5n4vU3dxTKS":
+        Script32();
+        break;
+      case "5qV2aeA6ZWh":
+        Script33();
+        break;
+      case "5VQvm3KmoFL":
+        Script34();
+        break;
+      case "5c5wzffPetS":
+        Script35();
+        break;
+      case "667bMFfaYN7":
+        Script36();
+        break;
+      case "63UivfR3Zjc":
+        Script37();
+        break;
+      case "6VMISX7k3xZ":
+        Script38();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script2()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script3()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script4()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script5()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script6()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script7()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script8()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script9()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script10()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script11()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script12()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script13()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script14()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script15()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script16()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script17()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script18()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script19()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script20()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script21()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script22()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script23()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script24()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script25()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script26()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script27()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script28()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script29()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script30()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script31()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script32()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script33()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script34()
+{
+  var player=GetPlayer();
+var STLpagetitle=player.GetVar("PageTitle");
+gtag('event', 'page_view', {page_title: STLpagetitle});
+}
+
+function Script35()
+{
+  gtag('event', 'click', {
+  'event_category' : 'userclicks',
+  'event_label' : 'OSSIX Bone'
+});
+}
+
+function Script36()
+{
+  gtag('send', 'event', 'Button', 'click', 'OSSIX Volumax');
+}
+
+function Script37()
+{
+  ga('config', 'event', 'Button', 'click', 'OSSIX Plus');
+}
+
+function Script38()
+{
+  ga('send', 'event', 'Button', 'click', 'GLYMATRIX');
+}
+
